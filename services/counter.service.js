74b688f24315a77d5e745bea9249caf65716f71c
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 16:40:58 +0100

    full slice to frontend created with error validation and return of mockData

--- services/counter.service.js
+++ services/counter.service.js
@@ -2,38 +2,49 @@
 
 const urlValidator = require("../helperFunctions/validation");
 
+const mockData = {
+    webPageUrl: "mockUrl",
+    totalWordCount: 10,
+    destructuredWordCount: [
+        { word: "the", count: 100 },
+        { word: "and", count: 90 },
+        { word: "umbrella", count: 2 },
+    ],
+};
+
+
 module.exports = {
-	name: "counter",
+    name: "counter",
 
-	settings: {},
+    settings: {},
 
-	dependencies: [],
+    dependencies: [],
 
-	actions: {
-		count: {
-			rest: {
-				method: "GET",
-				path: "/counter"
-			},
-			params: {
-				webPageUrl: "string"
-			},
-			async handler(ctx) {
+    actions: {
+        count: {
+            rest: {
+                method: "GET",
+                path: "/counter"
+            },
+            params: {
+                webPageUrl: "string"
+            },
+            async handler(ctx) {
                 if (!urlValidator.isValidURL(ctx.params.webPageUrl)) {
                     return `Error! Invalid URL: ${ctx.params.webPageUrl}.`;
                 }
-				return `Requested URL: ${ctx.params.webPageUrl}`;
-			}
-		},
-	},
+                return mockData;
+            }
+        },
+    },
 
-	events: {},
+    events: {},
 
-	methods: {},
+    methods: {},
 
-	created() {},
+    created() { },
 
-	async started() {},
+    async started() { },
 
-	async stopped() {}
+    async stopped() { }
 };
