7eeab4a9513997645c4b4830c0123a88f150d543
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-03 17:13:20 +0100

    added intial cheerio code + established successful response of word counr back to frontend

--- services/counter.service.js
+++ services/counter.service.js
@@ -1,17 +1,18 @@
 "use strict";
 
+const axios = require("axios");
+const cheerio = require("cheerio");
 const urlValidator = require("../helperFunctions/validation");
 
-const mockData = {
-    webPageUrl: "mockUrl",
-    totalWordCount: 10,
-    destructuredWordCount: [
-        { word: "the", count: 100 },
-        { word: "and", count: 90 },
-        { word: "umbrella", count: 2 },
-    ],
-};
-
+//const mockData = {
+//    webPageUrl: "mockUrl",
+//    totalWordCount: 10,
+//    destructuredWordCount: [
+//        { word: "the", count: 100 },
+//        { word: "and", count: 90 },
+//        { word: "umbrella", count: 2 },
+//    ],
+//};
 
 module.exports = {
     name: "counter",
@@ -33,7 +34,30 @@ module.exports = {
                 if (!urlValidator.isValidURL(ctx.params.webPageUrl)) {
                     return `Error! Invalid URL: ${ctx.params.webPageUrl}.`;
                 }
-                return mockData;
+
+                try {
+                    const response = await axios.get(ctx.params.webPageUrl);
+                    console.log(response);
+                    const html = response.data;
+                    console.log(html);
+
+                    const $ = cheerio.load(html);
+                    const text = $.text();
+                    const words = text.split(/\s+/);
+                    console.log(words);
+                    const wordCount = words.length;
+
+                    const result = {
+                        webPageUrl: ctx.params.webPageUrl,
+                        totalWordCount: wordCount,
+                        destructuredWordCount: [],//TO_DO: word count
+                    };
+
+                    return result;
+                } catch (error) {
+                    console.log(error);
+                    return `Error! Failed to fetch URL: ${ctx.params.webPageUrl}.`;
+                }
             }
         },
     },
@@ -48,3 +72,4 @@ module.exports = {
 
     async stopped() { }
 };
+
