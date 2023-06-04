b86f9047eaeb9cde51f6ce8e8e091c26fbdeba62
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-04 17:37:06 +0100

    updated error handling to throw errors properly

--- services/counter.service.js
+++ services/counter.service.js
@@ -4,6 +4,20 @@ const axios = require("axios");
 const urlValidator = require("../helperFunctions/validation");
 const countInformation = require("../helperFunctions/counting");
 
+class PrefacedUrlError extends Error {
+    constructor() {
+        super("Error! URL must be prefaced with http:// or https://");
+        this.name = "PrefacedUrlError";
+    }
+}
+
+class InvalidURLError extends Error {
+    constructor(url) {
+        super(`Error! Invalid URL: ${url}`);
+        this.name = "InvalidURLError";
+    }
+}
+
 module.exports = {
     name: "counter",
 
@@ -22,19 +36,18 @@ module.exports = {
             },
             async handler(ctx) {
                 if (!urlValidator.isPrefacedUrl(ctx.params.webPageUrl)) {
-                    return "Error! URL must be prefaced with http:// or https://";
+                    throw new PrefacedUrlError();
                 }
                 if (!urlValidator.isValidURL(ctx.params.webPageUrl)) {
-                    return `Error! Invalid URL: ${ctx.params.webPageUrl}`;
+                    throw new InvalidURLError(ctx.params.webPageUrl);
                 }
 
                 try {
                     const response = await axios.get(ctx.params.webPageUrl);
-
                     return countInformation.getCountInformation(ctx.params.webPageUrl, response);
                 } catch (error) {
-                    console.log(error);
-                    return `Error! Failed to fetch URL: ${ctx.params.webPageUrl}`;
+                    console.log(error.message);
+                    return error.message;
                 }
             }
         },
