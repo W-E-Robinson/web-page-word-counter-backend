3afd2dbe48070510651f2003bbe4d4ebf3c2aa18
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-04 15:38:43 +0100

    added counting tests

--- test/unit/helperFunctions/validation.spec.js
+++ test/unit/helperFunctions/validation.spec.js
@@ -1,31 +1,23 @@
 const { isPrefacedUrl, isValidURL } = require("../../../helperFunctions/validation");
 
 describe("isPrefacedUrl", () => {
-    test("returns true for a URL with http:// prefix", () => {
-        const result = isPrefacedUrl("http://example.com");
-        expect(result).toBe(true);
-    });
-
-    test("returns true for a URL with https:// prefix", () => {
-        const result = isPrefacedUrl("https://example.com");
-        expect(result).toBe(true);
-    });
-
-    test("returns false for a URL without http:// or https:// prefix", () => {
-        const result = isPrefacedUrl("example.com");
-        expect(result).toBe(false);
+    test.each([
+        ["http://example.com", true],
+        ["https://example.com", true],
+        ["example.com", false],
+    ])("returns true for a URL with http:// or https:// prefix", (url, expected) => {
+        const result = isPrefacedUrl(url);
+        expect(result).toBe(expected);
     });
 });
 
 describe("isValidURL", () => {
-    test("returns true for a valid URL", () => {
-        const result = isValidURL("https://example.com");
-        expect(result).toBe(true);
-    });
-
-    test("returns false for an invalid URL", () => {
-        const result = isValidURL("invalid-url");
-        expect(result).toBe(false);
+    test.each([
+        ["https://example.com", true],
+        ["invalid-url", false],
+    ])("returns true for a valid URL", (url, expected) => {
+        const result = isValidURL(url);
+        expect(result).toBe(expected);
     });
 });
 
