7000266031fe27b5a1efbf702cd6c8dca995c7db
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-21 19:20:18 +0100

    updated test each to have breakdowns for each happy path test, need to check accuracy and then begin TDD

--- test/unit/services/counter/counter.spec.js
+++ test/unit/services/counter/counter.spec.js
@@ -25,10 +25,73 @@ describe("Test 'counter' service", () => {
             });
 
             test.each([
-                ["./mocks/sample1.html", 27],
-                ["./mocks/sample3.html", 18],
-                ["./mocks/sample2.html", 18],
-            ])("should return word count and breakdown for %s", async (webPageUrl, expectedWordCount) => {
+                [
+                    "./mocks/sample1.html",
+                    22,
+                    [
+                        { word: "sample", count: 3 },
+                        { word: "text", count: 3 },
+                        { word: "html", count: 2 },
+                        { word: "welcome", count: 1 },
+                        { word: "to", count: 1 },
+                        { word: "1", count: 1 },
+                        { word: "this", count: 1 },
+                        { word: "is", count: 1 },
+                        { word: "the", count: 1 },
+                        { word: "content", count: 1 },
+                        { word: "of", count: 1 },
+                        { word: "it", count: 1 },
+                        { word: "contains", count: 1 },
+                        { word: "some", count: 1 },
+                        { word: "for", count: 1 },
+                        { word: "testing", count: 1 },
+                        { word: "purposes", count: 1 },
+                    ],
+                ],
+                [
+                    "./mocks/sample2.html",
+                    20,
+                    [
+                        { word: "welcome", count: 1 },
+                        { word: "to", count: 1 },
+                        { word: "sample", count: 2 },
+                        { word: "html", count: 2 },
+                        { word: "2", count: 1 },
+                        { word: "this", count: 1 },
+                        { word: "is", count: 1 },
+                        { word: "the", count: 1 },
+                        { word: "content", count: 1 },
+                        { word: "of", count: 1 },
+                        { word: "it", count: 1 },
+                        { word: "contains", count: 1 },
+                        { word: "some", count: 1 },
+                        { word: "additional", count: 1 },
+                        { word: "text", count: 3 },
+                    ],
+                ],
+                [
+                    "./mocks/sample3.html",
+                    21,
+                    [
+                        { word: "welcome", count: 1 },
+                        { word: "to", count: 1 },
+                        { word: "sample", count: 2 },
+                        { word: "html", count: 2 },
+                        { word: "3", count: 1 },
+                        { word: "this", count: 1 },
+                        { word: "is", count: 1 },
+                        { word: "the", count: 1 },
+                        { word: "content", count: 1 },
+                        { word: "of", count: 1 },
+                        { word: "it", count: 1 },
+                        { word: "includes", count: 1 },
+                        { word: "some", count: 1 },
+                        { word: "different", count: 1 },
+                        { word: "sample", count: 3 },
+                        { word: "text", count: 3 },
+                    ],
+                ],
+            ])("should return word count and breakdown for %s", async (webPageUrl, expectedWordCount, expectedDestructuredWordCount) => {
                 const htmlContent = fs.readFileSync(path.resolve(__dirname, webPageUrl), "utf-8");
                 axios.get.mockResolvedValueOnce({
                     data: htmlContent,
@@ -36,11 +99,12 @@ describe("Test 'counter' service", () => {
 
                 const response = await broker.call("counter.count", { webPageUrl });
                 expect(response.totalWordCount).toBe(expectedWordCount);
+                expect(response.destructuredWordCount).toEqual(expectedDestructuredWordCount);
             });
         });
 
         describe("Unhappy paths", () => {
-            test("should return an error when URL is not prefaced with http:// or https://", async () => {
+            test("should return an error when URL is not prefaced with http: or https:", async () => {
                 urlValidator.isPrefacedUrl.mockReturnValueOnce(false);
 
                 const response = await broker.call("counter.count", { webPageUrl: "www.google.com" });
@@ -49,8 +113,8 @@ describe("Test 'counter' service", () => {
             test("should return an error when URL is not valid", async () => {
                 urlValidator.isValidURL.mockReturnValueOnce(false);
 
-                const response = await broker.call("counter.count", { webPageUrl: "https:// www.google.com" });
-                expect(response).toBe("Error! Invalid URL: https:// www.google.com");
+                const response = await broker.call("counter.count", { webPageUrl: "https: www.google.com" });
+                expect(response).toBe("Error! Invalid URL: https: www.google.com");
             });
         });
     });
