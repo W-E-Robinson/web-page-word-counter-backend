bd24da9c3e01cf797c982bec24907aa800f64ae3
Will Robinson <william.ellis.robinson@gmail.com>
2023-06-21 18:51:53 +0100

    turned happy path into test each + mocked for unhappy paths

--- test/unit/services/counter/counter.spec.js
+++ test/unit/services/counter/counter.spec.js
@@ -1,11 +1,14 @@
 "use strict";
 
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
 const { ServiceBroker } = require("moleculer");
 const CounterService = require("../../../../services/counter.service");
 const urlValidator = require("../../../../helperFunctions/validation");
 jest.mock("../../../../helperFunctions/validation");
+jest.mock("axios");
 
 describe("Test 'counter' service", () => {
     let broker = new ServiceBroker({ logger: false });
@@ -16,37 +19,36 @@ describe("Test 'counter' service", () => {
 
     describe("Test 'counter.count' action", () => {
         describe("Happy path", () => {
-            test("should return word count for sample1.html", async () => {
+            beforeEach(() => {
                 urlValidator.isPrefacedUrl.mockReturnValue(true);
                 urlValidator.isValidURL.mockReturnValue(true);
+            });
 
-                const htmlContent = fs.readFileSync(path.resolve(__dirname, "./mocks/sample1.html"), "utf-8");
+            test.each([
+                ["./mocks/sample1.html", 18],
+                ["./mocks/sample3.html", 18],
+                ["./mocks/sample2.html", 18],
+            ])("should return word count and breakdown for %s", async (webPageUrl, expectedWordCount) => {
+                const htmlContent = fs.readFileSync(path.resolve(__dirname, webPageUrl), "utf-8");
                 axios.get.mockResolvedValueOnce({
-                    data: htmlContent
+                    data: htmlContent,
                 });
 
-                const response = await broker.call("counter.count", { webPageUrl: "./mocks/sample1.html" });
-                console.log(response);
-                expect(response.totalWordCount).toBe(18);
-            });
-
-            xtest("should return word count for sample2.html", async () => {
-                const response = await broker.call("counter.count", { webPageUrl: "./mocks/sample3.html" });
-                expect(response.totalWordCount).toBe(18);
-            });
-
-            xtest("should return word count for sample3.html", async () => {
-                const response = await broker.call("counter.count", { webPageUrl: "./mocks/sample2.html" });
-                expect(response.totalWordCount).toBe(18);
+                const response = await broker.call("counter.count", { webPageUrl });
+                expect(response.totalWordCount).toBe(expectedWordCount);
             });
         });
 
         describe("Unhappy paths", () => {
             test("should return an error when URL is not prefaced with http:// or https://", async () => {
+                urlValidator.isPrefacedUrl.mockReturnValueOnce(false);
+
                 const response = await broker.call("counter.count", { webPageUrl: "www.google.com" });
                 expect(response).toBe("Error! URL must be prefaced with http:// or https://");
             });
             test("should return an error when URL is not valid", async () => {
+                urlValidator.isValidURL.mockReturnValueOnce(false);
+
                 const response = await broker.call("counter.count", { webPageUrl: "https:// www.google.com" });
                 expect(response).toBe("Error! Invalid URL: https:// www.google.com");
             });
